21b509f53fb8ccc9195642aab79251cbf555fdf8
Nesi Mi <44912225+nesivmi@users.noreply.github.com>
2019-09-02 20:54:03 +0300

    Create main.py
    Utilized resources ;
    https://github.com/YogurtTheHorse/ulauncher-translator
    https://github.com/mouuff/mtranslate

---
+++ main.py
@@ -0,0 +1,100 @@
+# Utilized resources
+# https://github.com/YogurtTheHorse/ulauncher-translator
+# https://github.com/mouuff/mtranslate
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+import textwrap
+import sys
+import re
+
+if (sys.version_info[0] < 3):
+    import urllib2
+    import urllib
+    import HTMLParser
+else:
+    import html.parser
+    import urllib.request
+    import urllib.parse
+
+agent = {'User-Agent': "Mozilla/5.0 (Android 9; Mobile; rv:67.0.3) Gecko/67.0.3 Firefox/67.0.3"}
+
+
+def unescape(text):
+    if (sys.version_info[0] < 3):
+        parser = HTMLParser.HTMLParser()
+    else:
+        parser = html.parser.HTMLParser()
+    return (parser.unescape(text))
+
+
+def translate(to_translate, to_language="auto", from_language="auto", wrap_len="80"):
+    base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
+    if (sys.version_info[0] < 3):
+        to_translate = urllib.quote_plus(to_translate)
+        link = base_link % (to_language, from_language, to_translate)
+        request = urllib2.Request(link, headers=agent)
+        raw_data = urllib2.urlopen(request).read()
+    else:
+        to_translate = urllib.parse.quote(to_translate)
+        link = base_link % (to_language, from_language, to_translate)
+        request = urllib.request.Request(link, headers=agent)
+        raw_data = urllib.request.urlopen(request).read()
+    data = raw_data.decode("utf-8")
+    expr = r'class="t0">(.*?)<'
+    re_result = re.findall(expr, data)
+    if (len(re_result) == 0):
+        result = ""
+    else:
+        result = unescape(re_result[0])
+    
+    return ("\n".join(textwrap.wrap(result, int(wrap_len) if wrap_len.isdigit() else 80 )))
+
+
+
+
+class TranslateExtension(Extension):
+    def __init__(self):
+        super(TranslateExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        query = event.get_argument() or str()
+        
+        if len(query.strip()) == 0:
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/icon.png',
+                                    name='No input',
+                                    on_enter=HideWindowAction())
+            ])
+        
+        if len(query)>3 and ":" in query[0]:
+            from_language = "auto"
+            to_language = query[1:3]
+            query = query[3:]
+        elif len(query)>5 and ":" in query[2]:
+            from_language = query[:2]
+            to_language = query[3:5]
+            query = query[5:]
+        else:
+            from_language = extension.preferences["otherlang"]
+            to_language = extension.preferences["mainlang"]
+        
+        items = [
+            ExtensionResultItem(icon='images/icon.png',
+                                name=query,
+                                description=translate(query, to_language, from_language, extension.preferences["wrap"]),
+                                on_enter=HideWindowAction())
+        ]
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    TranslateExtension().run()
